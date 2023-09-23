c3a17235782eb2894b52376e1ea8661be96e8716
Olha Andriiets <osandriets@gmail.com>
2023-09-23 12:04:52 +0200

    init charts

--- src/app/components/chart/chart.module.ts
+++ src/app/components/chart/chart.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartComponent } from "./chart.component";
+import { BarChartComponent } from "./bar-chart/bar-chart.component";
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 
 
 @NgModule({
   declarations: [
     ChartComponent,
+    BarChartComponent,
+    PieChartComponent,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
     ChartComponent,
+    BarChartComponent,
+    PieChartComponent,
   ]
 })
 export class ChartModule { }
